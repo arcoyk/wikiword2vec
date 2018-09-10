9eb905a70b1e84b651965223e80d3821e7e0d349
yui <yuikita21@gmail.com>
2018-09-10 19:32:32 +0900

    fix

--- pickle_test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import random as rd
-
-data = {}
-with open('data/t_a.pickle', mode='rb') as f:
-  data = pickle.load(f)
-
-for k in rd.sample(data.keys(), 10):
-  print(k, data[k][:5])
